a6bbe9a932d9d437f38663dbe1ad093e80d2aa1e
John Morris <johnmorris4@bellsouth.net>
2018-01-27 07:10:02 -0500

    Finished setting up the Shopping Basket. Still need to setup additional Methods for the Basket

--- src/com/johnmorris/Main.java
+++ src/com/johnmorris/Main.java
@@ -1,8 +1,59 @@
 package com.johnmorris;
 
-public class Main {
+import java.util.Map;
 
+public class Main {
+    private static StockList stockList = new StockList();
     public static void main(String[] args) {
-	// write your code here
+	    StockItem temp = new StockItem("Bread", 1.27, 46);
+	    stockList.addStock(temp);
+
+	    temp = new StockItem("Quarters", 1.16, 500);
+	    stockList.addStock(temp);
+
+        temp = new StockItem("Chili", 1.25, 24);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Salt", .16, 24);
+        stockList.addStock(temp);
+        temp = new StockItem("Salt", .25, 12);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Cheese, Shredded", 6.25, 40);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Tea, Iced", .47, 97);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Coffee, Ground", .37, 250);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+
+        Basket johnsBasket = new Basket("John");
+        sellItem(johnsBasket, "Chili", 5);
+        sellItem(johnsBasket, "Bread", 30);
+        sellItem(johnsBasket, "Salt", 12);
+        //System.out.println(johnsBasket);
+        sellItem(johnsBasket, "Spanner", 1);
+        System.out.println(johnsBasket);
+        System.out.println(stockList);
+
+        for(Map.Entry<String, Double> price: stockList.PriceList().entrySet()) {
+            System.out.println(price.getKey() + " costs " + price.getValue());
+        }
+    }
+    public static int sellItem(Basket basket, String item, int quantity) {
+        //retrieve item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("\nSorry but the " + item +" is not available at this time" );
+            return 0;
+        }
+        if(stockList.sellStock(item, quantity) != 0) {
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
     }
 }
---
+++ src/com/johnmorris/MainChallenge.java
@@ -0,0 +1,10 @@
+package com.johnmorris;
+
+/**
+ * Created by jmorris on 1/27/18
+ */
+public class MainChallenge {
+    public static void main(String[] args) {
+
+    }
+}
